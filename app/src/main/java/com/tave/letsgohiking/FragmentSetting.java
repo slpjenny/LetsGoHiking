d16f22b57814d38aa8b27e30f96ced47eb3ec6dd
woongjichoi <allison1998@naver.com>
2020-09-17 03:57:03 +0900

    환경설정

--- app/src/main/java/com/tave/letsgohiking/FragmentSetting.java
+++ app/src/main/java/com/tave/letsgohiking/FragmentSetting.java
@@ -1,9 +1,11 @@
 package com.tave.letsgohiking;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +13,18 @@ import androidx.fragment.app.Fragment;
 
 public class FragmentSetting extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_setting, container, false);
+
+        Button google_logoutBtn=rootView.findViewById(R.id.google_logoutBtn);
+        google_logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return rootView;
+
     }
 
 }
