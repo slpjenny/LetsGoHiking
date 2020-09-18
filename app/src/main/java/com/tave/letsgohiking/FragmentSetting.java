7ba4db889896835f8288603ec6c6cac5e4298b65
woongjichoi <allison1998@naver.com>
2020-09-18 22:07:52 +0900

    FragmentSetting 로그아웃 구현

--- app/src/main/java/com/tave/letsgohiking/FragmentSetting.java
+++ app/src/main/java/com/tave/letsgohiking/FragmentSetting.java
@@ -11,20 +11,31 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FragmentSetting extends Fragment {
+    private FirebaseAuth mAuth;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_setting, container, false);
+
+        mAuth=FirebaseAuth.getInstance();
 
         Button google_logoutBtn=rootView.findViewById(R.id.google_logoutBtn);
         google_logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                signOut();
 
             }
         });
 
         return rootView;
 
+    }
+
+    private void signOut(){
+        FirebaseAuth.getInstance().signOut();
     }
 
 }
