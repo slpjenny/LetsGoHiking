909966d1789a7b87ea5322e6bc5ffa243822552a
woongjichoi <allison1998@naver.com>
2020-09-21 21:55:26 +0900

    환경설정 구글 프로필 띄우기

--- app/src/main/java/com/tave/letsgohiking/FragmentSetting.java
+++ app/src/main/java/com/tave/letsgohiking/FragmentSetting.java
@@ -1,6 +1,8 @@
 package com.tave.letsgohiking;
 
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +14,74 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FragmentSetting extends Fragment {
     private FirebaseAuth mAuth;
+    Bitmap bitmap;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_setting, container, false);
 
         mAuth=FirebaseAuth.getInstance();
+        final FirebaseUser user=mAuth.getCurrentUser();
+
+        CircleImageView user_profile=rootView.findViewById(R.id.user_profile);
+        Thread mThread=new Thread(){
+            @Override
+            public void run() {
+                try{
+
+                    URL url = new URL(user.getPhotoUrl().toString());
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setDoInput(true);
+
+                    conn.connect();
+
+
+                    InputStream is = conn.getInputStream();
+
+                    bitmap = BitmapFactory.decodeStream(is);
+
+                } catch (MalformedURLException ee) {
+
+                    ee.printStackTrace();
+
+                }catch (IOException e){
+
+                    e.printStackTrace();
+
+                }
+
+            }
+        };
+
+        mThread.start();
+
+
+        try{
+
+            mThread.join();
+
+            user_profile.setImageBitmap(bitmap);
+
+        }catch (InterruptedException e){
+
+            e.printStackTrace();
+
+        }
+
+
 
         Button google_logoutBtn=rootView.findViewById(R.id.google_logoutBtn);
         google_logoutBtn.setOnClickListener(new View.OnClickListener() {
