a38cb9c6ef5859d72bba399e24bfe0962dd85eb4
hanzzb <hellojione2@gmail.com>
2020-09-28 23:38:47 +0900

    measureActivity 전환 구현

--- app/src/main/java/com/tave/letsgohiking/MeasureActivity.java
+++ app/src/main/java/com/tave/letsgohiking/MeasureActivity.java
@@ -1,6 +1,9 @@
 package com.tave.letsgohiking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class MeasureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measure);
+        ImageButton mapBtn = findViewById(R.id.mapBtn);
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT); //Task 내에 이미 활성화된 activity 를 다시 활성화 할때,
+                                                                        //새로 생성하지 않고 재사용하는 flag
+                startActivity(intent);
+            }
+        });
     }
 }
